36a61f7150a63a1fb4d96ac7edd91979c403398a
Rabeeh Rabiee Abdelatty <65160300+rabeh74@users.noreply.github.com>
2022-03-20 10:34:09 +0200

    task-completed

---
+++ NotYetSetException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task1;
+
+/**
+ *
+ * @author rabeh rabie
+ */
+public class NotYetSetException extends Exception {
+
+    public NotYetSetException(String s) {
+        super(s);
+    }
+}
---
+++ Quizes.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task1;
+
+/**
+ *
+ * @author rabeh rabie
+ */
+public class Assignment {
+
+    
+    public static void main(String[] args) {
+        //Quiz1
+        
+        //1-String dog="BARK";
+        //2-mylist[2]
+        //3-1
+        //4-reverseName("jone","Doe")
+
+
+        //-------------------------------------------------------------------------------------
+
+
+        //Quiz2
+        /*1-
+                A-member variable
+                B-constructor
+                C-method
+                */
+        //2-Nothing is no such convention ,and such a variable is like any other
+        
+        //3-"New error" 
+        
+        /*4-
+                1-Test testobj= new Test("old")
+                2-testobj.setTest("new")
+        */
+
+
+        //---------------------------------------------------------------------------------------
+
+
+
+        //Quiz3
+        //1-This willnot compile because age isnot initialized
+        //2-4
+        //3-Hello-GO goo -Hello-hi
+        //4-Error becouse they arenot method 2 in myclass
+        //----------------------------------------------------------------------------------------- 
+        
+        
+    }
+    
+}
